1a485d712743c42cf89e4e2333a266aacdc52524
root <root@Lazulie.localdomain>
2021-08-01 20:55:37 -0700

    added one to many Project and Task routes

--- routes/api.php
+++ routes/api.php
@@ -18,24 +18,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/upcoming', function (){
-  $upcoming = \App\Models\Upcoming::all();
-
-  return \App\Http\Resources\UpcomingResource::collection($upcoming);
-});
-
-Route::post('/upcoming', function (Request $request){
-    return \App\Models\Upcoming::create([
-        'title' => $request->title,
-        'taskId' => $request->taskId,
-        'completed' => $request->completed
-    ]);
-});
-
-Route::delete('/upcoming/{taskId}', function ($taskId){
-   \Illuminate\Support\Facades\DB::table('upcomings')->where('taskId', $taskId)->delete();
-   return 204;
-});
+//Route::get('/upcoming', function (){
+//  $upcoming = \App\Models\Upcoming::all();
+//
+//  return \App\Http\Resources\UpcomingResource::collection($upcoming);
+//});
+//
+//Route::post('/upcoming', function (Request $request){
+//    return \App\Models\Upcoming::create([
+//        'title' => $request->title,
+//        'taskId' => $request->taskId,
+//        'completed' => $request->completed
+//    ]);
+//});
 
 Route::get('/projects', function (){
     $projects = \App\Models\Project::all();
@@ -43,26 +38,27 @@ Route::get('/projects', function (){
     return \App\Http\Resources\ProjectResource::collection($projects);
 });
 
-//Route::get('/projects', function (){
-//    $projects = \App\Models\Projects::all();
-//    $tasks = [];
-//    foreach ($projects as $project) {
-//        //$tasks.array_push($project);
-//        array_push($tasks, $project->upcomings());
-//    }
-//    return $project[1];
-//    //return \App\Http\Resources\ProjectsResource::collection($projects);
+Route::get('/project/{id}', function ($id){
+    $project = \App\Models\Project::query()->findOrFail($id);
+
+    return $project;
+});
+
+Route::get('/tasks/{id}', function ($id){
+    $project = \App\Models\Project::query()->findOrFail($id);
+    $tasks = $project->tasks;
+
+    return \App\Http\Resources\ProjectResource::collection($tasks);
+});
+
+
+Route::get('/task/{id}', function ($id){
+    $task = \App\Models\Task::query()->findOrFail($id);
+
+    return $task;
+});
+
+//Route::delete('/projects/{PID}', function ($PID){
+//    \Illuminate\Support\Facades\DB::table('projects')->where('PID', $PID)->delete();
+//    return 204;
 //});
-
-Route::post('/projects', function (Request $request){
-    return \App\Models\Project::create([
-        'title' => $request->title,
-        'PID' => $request->PID,
-        'completed' => $request->completed
-    ]);
-});
-
-Route::delete('/projects/{PID}', function ($PID){
-    \Illuminate\Support\Facades\DB::table('projects')->where('PID', $PID)->delete();
-    return 204;
-});
